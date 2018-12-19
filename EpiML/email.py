5967c7fb598d5fc5ae8ae19b844689becca99d5a
jchen67 <499690505@qq.com>
2018-12-19 12:29:09 -0500

    fix the processing link

--- EpiML/email.py
+++ EpiML/email.py
@@ -13,10 +13,10 @@ def send_submit_job_email(recipients, jobname, jobid, security_code):
 
     msg.body = 'Dear {},\nThank you for using EpiML web server.\n\nYour job (Job Name: {}) has been submitted. ' \
                'The processing time depends on the scale of your data and selected method. You can check job status ' \
-               'and retrieve result via the following link.\n Result Link: {}{}\n\n' \
+               'and retrieve result via the following link.\n Result Link: {}\n\n' \
                'Sincerely,\n' \
-               'ShiLab'.format(recipients[0], jobname, request.headers['Host'],
-                               url_for('processing', jobid=jobid, security_code=security_code))
+               'ShiLab'.format(recipients[0], jobname,
+                               url_for('processing', jobid=jobid, security_code=security_code, _external=True))
 
     # msg.html=render_template('notice_email.html')
     mail.send(msg)
@@ -30,10 +30,10 @@ def send_job_done_email(recipients, jobname, jobid, security_code):
     msg = Message(subject, sender=sender, recipients=recipients)
 
     msg.body = 'Dear {},\nThank you for using EpiML web server.\n\nYYour job (Job Name: {}) has been done. ' \
-               'You can retrieve result via the following link\n Result Link: {}{}\n\n' \
+               'You can retrieve result via the following link\n Result Link: {}\n\n' \
                'Sincerely,\n' \
-               'ShiLab'.format(recipients[0], jobname, request.headers['Host'],
-                               url_for('processing', jobid=jobid, security_code=security_code))
+               'ShiLab'.format(recipients[0], jobname,
+                               url_for('processing', jobid=jobid, security_code=security_code, _external=True))
 
     # msg.html=render_template('notice_email.html')
     mail.send(msg)
@@ -47,10 +47,10 @@ def send_job_error_email(recipients, jobname, jobid, security_code):
     msg = Message(subject, sender=sender, recipients=recipients)
 
     msg.body = 'Dear {},\nThank you for using EpiML web server.\n\nYYour job (Job Name: {}) is error. ' \
-               'You can check error via the following link\n Result Link: {}{}\n\n' \
+               'You can check error via the following link\n Result Link: {}\n\n' \
                'Sincerely,\n' \
-               'ShiLab'.format(recipients[0], jobname, request.headers['Host'],
-                               url_for('processing', jobid=jobid, security_code=security_code))
+               'ShiLab'.format(recipients[0], jobname,
+                               url_for('processing', jobid=jobid, security_code=security_code, _external=True))
 
     # msg.html=render_template('notice_email.html')
     mail.send(msg)
