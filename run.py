26ecab12275f9e5438444965e8529bd56e21431f
jchen67 <499690505@qq.com>
2019-03-20 14:33:04 -0400

    http

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc', host='0.0.0.0', port=80)
+    # app.run(ssl_context=('cert.pem','key.pem'), host='0.0.0.0', port=80)
+    app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=80)
