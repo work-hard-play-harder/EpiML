aa7a2dd310e773070e3e668b2b2c1babe1c134e3
jchen67 <499690505@qq.com>
2019-03-20 14:33:31 -0400

    http

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ def make_shell_context():
 
 if __name__ == '__main__':
     # app.run(ssl_context=('cert.pem','key.pem'), host='0.0.0.0', port=80)
-    app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
